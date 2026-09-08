b97065bb208e911d85bbc15aac6108a936c42866
unknown <kan077bct041@kec.edu.np>
2026-09-08 21:42:23 +0545

    variables set for best practices

---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for subnets"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for Amazon Linux 2023"
+  type        = string
+  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2023 in us-east-1
+}
+
+variable "my_ip" {
+  description = "Your IP address for SSH access"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "cafe-lab"
+}
