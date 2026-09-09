a64f4239acdc1c7fbbfa83e5f9672462251cfaf3
unknown <kan077bct041@kec.edu.np>
2026-09-09 14:56:06 +0545

    minor change

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "ec2" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
-  key_name               = var.key_name
-  user_data              = var.user_data
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = var.security_group_ids
+  key_name                    = var.key_name
+  user_data                   = var.user_data
   associate_public_ip_address = var.associate_public_ip
 
   root_block_device {
